d32a3ec7988f79e81a72e3f9dcb054787afc855d
FelipeEmanuel <felipe.nunes@ccc.ufcg.edu.br>
2022-11-26 19:06:13 -0300

    Versão 2.0.1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -89,13 +89,15 @@ function Dashboard() {
         <br/>
         <h3>Você pode palpitar até 15 minutos antes do início do jogo!</h3>
         <h3 className='alerta'>INSTRUÇÕES:</h3>
-        <h3 className='alerta'>- É necessário palpitar em um jogo por vez, a página atualizará quando você realizar um palpite!</h3>
-        <h3 className='alerta'>- Os jogos em que você realizou um palpite aparecem na lateral, se algum jogo não estiver lá, você ainda não palpitou nele!</h3>
+        <h3 className='alerta'>- Caso a lista de jogos não esteja aparecendo, saia e faça seu login novamente, muito provavelmente há uma nova versão do site no ar!</h3>
+        <h3 className='alerta'>- É necessário palpitar em um jogo por vez, um aviso em verde com a mensagem "Palpite Realizado" aparecerá embaixo do botão de confirmar palpite e o palpite também aparecerá na lateral!</h3>
         <h3 className='alerta'>- É possível trocar seu palpite, é só enviar o palpite novamente, como se fosse a primeira vez, o seu palpite atualizado aparecerá na lateral!</h3>
+        <h3 className='alerta'>- Quando você realizar um palpite os placares que você enviou ficarão salvos na caixinha mostrando o palpite que você realizou, se a caixinha de enviar os palpites estiver vazia é porquê você não fez um palpite ainda!</h3>
         <h2>Jogos</h2>
       </section>
       <div className='palpitesgrid'>
         <section className='contentpalpites'>
+          <h2>Jogos disponíveis para palpitar no momento!</h2>
           { 
             games?.length > 0 && 
             <div className='palpites'>
