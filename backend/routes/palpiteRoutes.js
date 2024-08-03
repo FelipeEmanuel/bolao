53801f0bf8e6791fc402d263f02c4ca857c58f1c
Felipe Emanuel <40031635+FelipeEmanuel@users.noreply.github.com>
2024-08-03 05:25:37 -0300

    Update palpiteRoutes.js

--- backend/routes/palpiteRoutes.js
+++ backend/routes/palpiteRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getPartidas, setPalpite, getUserPalpites, userPalpitesById} = require('../controllers/palpiteController')
+const {setPalpite, getPalpitesEsports, getPalpitesFutebol, userPalpitesById} = require('../controllers/palpiteController')
 const { protect, isAdmin } = require('../middleware/authMiddleware')
 
-router.route('/').get(protect, getPartidas)
 router.route('/').post(protect, setPalpite)
-router.route('/userPalpites').get(protect, getUserPalpites)
+router.route('/futebol').get(protect, getPalpitesFutebol)
+router.route('/esports').get(protect, getPalpitesEsports)
 router.route('/get/:id').get(protect, isAdmin, userPalpitesById)
 
 module.exports = router
