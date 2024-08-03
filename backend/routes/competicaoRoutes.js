9bb1b91c96abc04edb6a52c576dde0220358bd2b
Felipe Emanuel <40031635+FelipeEmanuel@users.noreply.github.com>
2024-08-03 05:25:12 -0300

    Update competicaoRoutes.js

--- backend/routes/competicaoRoutes.js
+++ backend/routes/competicaoRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getCompeticao, setCompeticoes, updateCompeticao, deleteCompeticao, getCompeticaoById, encerrarCompeticao} = require('../controllers/competicaoController')
+const {getCompeticao, setCompeticoes, updateCompeticao, deleteCompeticao, getCompeticaoById, encerrarCompeticao, getCompeticoesAtivas} = require('../controllers/competicaoController')
 const { protect, isAdmin } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getCompeticao).post(protect, isAdmin, setCompeticoes)
-router.route('/:id').delete(protect, isAdmin, deleteCompeticao).put(protect, isAdmin, updateCompeticao).get(protect, getCompeticaoById)
+router.route('/comps/:id').delete(protect, isAdmin, deleteCompeticao).put(protect, isAdmin, updateCompeticao).get(protect, getCompeticaoById)
 router.route('/encerrar/:id').put(protect, isAdmin, encerrarCompeticao)
+router.route('/ativas').get(protect, getCompeticoesAtivas)
  
 module.exports = router
