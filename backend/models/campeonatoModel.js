5d70059eb1ff0e46deb0e0c394787432c0910bcb
Felipe Emanuel <40031635+FelipeEmanuel@users.noreply.github.com>
2024-08-03 05:23:40 -0300

    Update campeonatoModel.js

--- backend/models/campeonatoModel.js
+++ backend/models/campeonatoModel.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const CampeonatoSchema = mongoose.Schema({
     name: {type: String, required: [true, 'Please add a name']},
     sigla: {type: String, required: [true, 'Please add a sigla']},
+    categoria: {type: String, required: true},
+    cor: {type: String, default: "#fff"}
 },{
     timestamps: true
 })
