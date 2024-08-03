cc08d3e8940c70e7bfb57d571902e5c486b5a598
Felipe Emanuel <40031635+FelipeEmanuel@users.noreply.github.com>
2024-08-03 05:21:31 -0300

    Update gameController.js

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,11 +1,80 @@
 const asyncHandler = require('express-async-handler')
 const Game = require('../models/gameModel')
 const Palpite = require('../models/palpiteModel')
+const Times = require('../models/timesModel')
+const Competicao = require('../models/competicaoModel')
+const { getCompeticoesPorCategoria } = require('./competicaoController')
+
+const getGamesPorCategoria = async (categoria, ativo = null, semanal = null, dataInicio = null, dataFim = null) => {
+    // Constrói o filtro match dinamicamente
+    const match = {
+        'campeonato.categoria': categoria
+    };
+
+    if (ativo !== null) {
+        match.ativo = ativo;
+    }
+
+    if (semanal !== null) {
+        match.semanal = semanal;
+    }
+
+    if (dataInicio !== null && dataFim !== null) {
+        match.dataLimite = { $gte: dataInicio, $lte: dataFim };
+    }
+
+    return await Game.aggregate([
+        {
+            $lookup: {
+                from: 'competicaos', // nome da coleção de competições
+                localField: 'competicao',
+                foreignField: '_id',
+                as: 'competicao'
+            }
+        },
+        {
+            $unwind: '$competicao'
+        },
+        {
+            $lookup: {
+                from: 'campeonatos', // nome da coleção de campeonatos
+                localField: 'competicao.campeonato',
+                foreignField: '_id',
+                as: 'campeonato'
+            }
+        },
+        {
+            $unwind: '$campeonato'
+        },
+        {
+            $match: match // Aplica o filtro construído dinamicamente
+        },
+        {
+            $sort: {
+                'createdAt': -1
+            }
+        },
+        {
+            $limit: 100
+        }
+    ]);
+};
 
 const getGames = asyncHandler(async (req, res) => {
-    const games = await Game.find().sort({'createdAt': -1}).limit(100)
+    //const gamesFutebol = await Game.find({categoria: "Futebol"}).sort({'createdAt': -1}).limit(100)
+    const gamesFutebol = await getGamesPorCategoria('Futebol');
 
-    res.status(200).json(games)
+    const gamesEsports = await getGamesPorCategoria('Esports');
+
+    const escudosFutebol = await Times.find({categoria: "Futebol", ativo: true})
+
+    const escudosEsports = await Times.find({categoria: "Esports", ativo: true})
+
+    const competicoesFutebol = await getCompeticoesPorCategoria('Futebol', true);
+
+    const competicoesEsports = await getCompeticoesPorCategoria('Esports', true);
+
+    res.status(200).json({gamesFutebol, gamesEsports, escudosFutebol, escudosEsports, competicoesFutebol, competicoesEsports})
 })
 
 const getGameById = asyncHandler(async (req, res) => {
@@ -22,7 +91,7 @@ const getGameById = asyncHandler(async (req, res) => {
 
 const setGames = asyncHandler(async (req, res) => {
 
-    const {time1, time2, placar1, placar2, competicao, dataLimite, isocodetime1, isocodetime2, infoCamp, infoJogo, infoGroup, gameType} = req.body
+    const {time1, time2, placar1, placar2, competicao, dataLimite, isocodetime1, isocodetime2, infoCamp, infoJogo, infoGroup, gameType, modelo} = req.body
 
     if (!time1 || !time2) {
         res.status(400)
@@ -34,7 +103,7 @@ const setGames = asyncHandler(async (req, res) => {
         user: req.user.id, competicao, 
         time1, time2, placar1, placar2,
         dataLimite, isocodetime1, isocodetime2, 
-        infoCamp, infoJogo, infoGroup, gameType
+        infoCamp, infoJogo, infoGroup, gameType, modelo
     })
 
     res.status(200).json(game)
@@ -62,7 +131,16 @@ const deleteGame = asyncHandler(async (req, res) => {
         console.log("Deu erro aqui", error);
     }
 
-    await Game.findByIdAndDelete(req.params.id, function (err, docs) {
+    try {
+        await Game.findByIdAndDelete(req.params.id)
+        const games = await Game.find().sort({'createdAt': -1}).limit(100)
+        res.status(200).json(games)
+    } catch (error) {
+        res.status(400)
+        throw new Error('Game not found')
+    }
+
+    /*await Game.findByIdAndDelete(req.params.id, function (err, docs) {
         if (err) {
             console.log(err)
             res.status(400)
@@ -70,12 +148,12 @@ const deleteGame = asyncHandler(async (req, res) => {
         }
         else {
             console.log("Deleted : ", docs);
-            res.status(200).json({id: req.params.id})
+            res.status(200).json("Ok")
         }
-    });
+    });*/
        
 })
 
 module.exports = {
-    getGames, getGameById, setGames, updateGame, deleteGame
+    getGames, getGameById, setGames, updateGame, deleteGame, getGamesPorCategoria
 }
