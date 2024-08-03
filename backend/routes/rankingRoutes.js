88f8e35f520f84399f8bfae0b852639c5e601309
Felipe Emanuel <40031635+FelipeEmanuel@users.noreply.github.com>
2024-08-03 05:25:56 -0300

    Update rankingRoutes.js

--- backend/routes/rankingRoutes.js
+++ backend/routes/rankingRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {getRanking, setPontuacao, criarRanking, getRankingSemanal} = require('../controllers/rankingController')
+const {getRanking, setPontuacao, criarRanking, getRankingSemanal, hallTitulos} = require('../controllers/rankingController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/getPontuacao/:id').get(protect, getRanking)
 router.route('/setPontuacao').put(protect, setPontuacao)
 router.route('/criarPontuacao/:id').post(protect, criarRanking)
 router.route('/semanal').get(protect, getRankingSemanal)
+router.route('/hall-titulos').get(protect, hallTitulos)
 
 module.exports = router
